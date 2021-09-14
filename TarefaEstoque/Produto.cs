704579537a1bccbe4125bf950206d8bff1534374
Alex Marcelo <lete_alex@hotmail.com>
2021-09-14 18:54:33 -0300

    Adicionar arquivos de projeto.

---
+++ TarefaEstoque/Produto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace TarefaEstoque
+{
+     class Produto
+    {
+        //Criada as variaveis necessarias
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+        //Responsavel pelos preços do usuario em estoque
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        //Responsavel por somar numeros do usuario
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        //Responsavel por subtrair numeros do usuario
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+
+        //Pega todas as informaçoes, e printa no console no final de todo o processo
+        public override string ToString()
+        {
+            return Nome
+                + " , R$"
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + " , "
+                + Quantidade 
+                + " unidades , Total: R$"
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+
+}
---
+++ TarefaEstoque/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace TarefaEstoque
+{
+   public class Program
+    {
+        static void Main(string[] arg)
+        {
+            //Instancia P como um novo produto
+            Produto p = new Produto();
+
+            //Pede para que o estoquista entre com os dados de cada produto
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade em estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+            //Fornece os dados do produto
+            Console.WriteLine("Dados do produto: " + p);
+            //Pede para que forneça dados de quantidade de produtos novos
+            Console.WriteLine("Digite o número de produtos a ser adicionado em estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            //Diz que os dados foram atualizados
+            Console.WriteLine("Dados atualizados: " + p);
+            //Pede para que o estoquista digite quantos produtos sairam do estoque
+            Console.WriteLine("Digite o número de produtos a serem removidos do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            //Diz que os dados foram atualizados
+            Console.WriteLine("Dados atualizados: " +p);
+        }
+    }
+}
