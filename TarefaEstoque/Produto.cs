c2ff2dd1967cea004b185c95d8c75b4dfdb90d97
Aleexmarcelo <jojoanete@gmail.com>
2021-09-22 19:48:38 -0300

    Troquei praticamente todo o código, adicionando 2 branchs e usando encapsulamento

--- TarefaEstoque/Produto.cs
+++ TarefaEstoque/Produto.cs
@@ -3,41 +3,73 @@ using System.Collections.Generic;
 using System.Text;
 using System.Globalization;
 
-namespace TarefaEstoque
+namespace estoque
 {
-     class Produto
+    class Produto
     {
-        //Criada as variaveis necessarias
-        public string Nome;
-        public double Preco;
-        public int Quantidade;
-        //Responsavel pelos preços do usuario em estoque
+        //Criada a variavél nome
+        private string _nome;
+        //Cria as variaveis que irei utilizar get e set
+        public double _preco { get; private set; }
+        public int _quantidade 
+        {    
+            get; private set;
+        }
+
+        //Construtor
+        public Produto (string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        //Construtor da variavel usando o get e set
+        public string Nome
+        {
+            
+            get
+            {
+                return _nome;
+            }       
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        //Função que calcula o valor do estoque
         public double ValorTotalEmEstoque()
         {
-            return Preco * Quantidade;
-        }
-        //Responsavel por somar numeros do usuario
-        public void AdicionarProdutos(int quantidade)
-        {
-            Quantidade += quantidade;
-        }
-        //Responsavel por subtrair numeros do usuario
-        public void RemoverProdutos(int quantidade)
-        {
-            Quantidade = Quantidade - quantidade;
+            return _preco * _quantidade;
         }
 
-        //Pega todas as informaçoes, e printa no console no final de todo o processo
+        //Função que adiciona produtos no estoque
+        public void AdicionarProduto(int qtd)
+        {
+            _quantidade += qtd;
+        }
+
+        //Função que remove produtos do estoque
+        public void RemoverProdutos(int qtd)
+        {
+            _quantidade = _quantidade - qtd;
+        }
+
+        //Retorno do console com todos os dados
         public override string ToString()
         {
-            return Nome
-                + " , R$"
-                + Preco.ToString("F2", CultureInfo.InvariantCulture)
-                + " , "
-                + Quantidade 
-                + " unidades , Total: R$"
-                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+            return _nome
+            + ",R$"
+            + _preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ","
+            + _quantidade
+            + " unidades,Total: R$"
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+
         }
     }
-
 }
